1b3911d5c1c306cb2acd1490e9c7bf38e6a9e2f0
RusTron <rusan.tron@gmail.com>
2020-09-21 23:18:42 +0300

    Kickstarter

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const form = document.querySelector('.questions__form.form');
+
+const inputs = document.querySelectorAll('.form__input.field');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  inputs
+    .forEach(input => {
+      input.value = '';
+    });
+});
