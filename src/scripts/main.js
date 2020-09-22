12a95212a6eac8e51ed74ebd4a8a26766d4de46d
RusTron <rusan.tron@gmail.com>
2020-09-23 01:10:49 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,11 +4,31 @@ const form = document.querySelector('.questions__form.form');
 
 const inputs = document.querySelectorAll('.form__input.field');
 
+const play = document.querySelector('.video__player-round');
+
+const video = document.querySelector('.video__player-movie');
+
+let active = false;
+
 form.addEventListener('submit', (event) => {
   event.preventDefault();
 
-  inputs
-    .forEach(input => {
-      input.value = '';
-    });
+  inputs.forEach(input => {
+    input.value = '';
+  });
+});
+
+play.addEventListener('click', () => {
+  if (!active) {
+    active = !active;
+    play.style = 'opacity: 0';
+    video.style = 'visibility: visible';
+    video.play();
+
+    return;
+  }
+
+  active = !active;
+  video.pause();
+  play.style = 'opacity: 1';
 });
